d167eb605a6a09e47f9339623c663a63c9d34645
Luke Leonard (Carly) <lukeleonard.coder@gmail.com>
2020-02-21 10:42:05 -0700

    added DB and User stub

---
+++ JPAFrameworkAutomation/src/main/java/com/skilldistillery/frameworkautomation/entities/User.java
@@ -0,0 +1,46 @@
+package com.skilldistillery.frameworkautomation.entities;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+public class User {
+	@Id
+	private Integer id;
+	private String username;
+	private String password;
+	private Boolean enabled;
+	private String role;
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public Boolean getEnabled() {
+		return enabled;
+	}
+	public void setEnabled(Boolean enabled) {
+		this.enabled = enabled;
+	}
+	public String getRole() {
+		return role;
+	}
+	public void setRole(String role) {
+		this.role = role;
+	}
+	
+	
+}
