b3f9e713b07eb21cea26b3de80f22b01ab27dde1
Matt <mra286@humboldt.edu>
2020-02-21 10:06:50 -0700

    Stub Framework setup grandle + spring boot

---
+++ FrameworkAutomation/src/main/java/com/skilldistillery/frameworkautomation/FrameworkAutomationApplication.java
@@ -0,0 +1,13 @@
+package com.skilldistillery.frameworkautomation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FrameworkAutomationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FrameworkAutomationApplication.class, args);
+	}
+
+}
---
+++ FrameworkAutomation/src/test/java/com/skilldistillery/frameworkautomation/FrameworkAutomationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skilldistillery.frameworkautomation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FrameworkAutomationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
