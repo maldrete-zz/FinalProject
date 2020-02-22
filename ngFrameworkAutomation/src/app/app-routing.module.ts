67f7d131df59a79ba14f9c918bb3c1fbd60eb328
Caleb Whitford <calebwhitford@Administrators-MacBook-Pro-2.local>
2020-02-22 14:28:29 -0700

    added angular connectivity/stubbed out components/router links

--- ngFrameworkAutomation/src/app/app-routing.module.ts
+++ ngFrameworkAutomation/src/app/app-routing.module.ts
@@ -1,8 +1,19 @@
+import { RegisterComponent } from './util/SignUp/register/register.component';
+import { CreateTemplateComponent } from './util/generator/create-template/create-template.component';
+import { GenerateComponent } from './util/generator/generate/generate.component';
+import { ResultsComponent } from './util/search/results/results.component';
+import { LandingPageComponent } from './util/home/landing-page/landing-page.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'home', component: LandingPageComponent},
+  {path: 'results', component: ResultsComponent},
+  {path: 'generator', component: GenerateComponent},
+  {path: 'creator', component: CreateTemplateComponent},
+  {path: 'register', component: RegisterComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
