bceb844830e895317680f0e8ec037de43843e257
Caleb Whitford <calebwhitford@Administrators-MacBook-Pro-2.local>
2020-02-22 12:10:44 -0700

    Transfered auth service code, created entities/components framework

--- ngFrameworkAutomation/src/app/app.module.ts
+++ ngFrameworkAutomation/src/app/app.module.ts
@@ -3,10 +3,26 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavigationComponent } from './util/navigation/navigation.component';
+import { FooterComponent } from './util/footer/footer.component';
+import { LandingPageComponent } from './util/home/landing-page/landing-page.component';
+import { RegisterComponent } from './util/SignUp/register/register.component';
+import { UserProfileComponent } from './util/user/user-profile/user-profile.component';
+import { ResultsComponent } from './util/search/results/results.component';
+import { GenerateComponent } from './util/generator/generate/generate.component';
+import { CreateTemplateComponent } from './util/generator/create-template/create-template.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavigationComponent,
+    FooterComponent,
+    LandingPageComponent,
+    RegisterComponent,
+    UserProfileComponent,
+    ResultsComponent,
+    GenerateComponent,
+    CreateTemplateComponent
   ],
   imports: [
     BrowserModule,
