d293fc8710327d7f3a475db3d8a1c7ee49ec3b27
Luke Leonard (Carly) <lukeleonard.coder@gmail.com>
2020-02-22 15:02:28 -0700

    fixed template import on repo

--- FrameworkAutomation/src/main/java/com/skilldistillery/frameworkautomation/repositories/TemplateRepository.java
+++ FrameworkAutomation/src/main/java/com/skilldistillery/frameworkautomation/repositories/TemplateRepository.java
@@ -1,7 +1,8 @@
 package com.skilldistillery.frameworkautomation.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import com.skilldistillery.frameworkautomation.entities.template;
+
+import com.skilldistillery.frameworkautomation.entities.Template;
 
 public interface TemplateRepository extends JpaRepository<Template, Integer> {
 
