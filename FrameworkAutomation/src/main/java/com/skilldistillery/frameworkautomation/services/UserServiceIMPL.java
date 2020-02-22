da3f8c7c560f1bd1d7557c76186abf4b7e10b1c6
Caleb Whitford <calebwhitford@Administrators-MacBook-Pro-2.local>
2020-02-22 15:55:13 -0700

    stubbed out services

--- FrameworkAutomation/src/main/java/com/skilldistillery/frameworkautomation/services/UserServiceIMPL.java
+++ FrameworkAutomation/src/main/java/com/skilldistillery/frameworkautomation/services/UserServiceIMPL.java
@@ -3,6 +3,7 @@ package com.skilldistillery.frameworkautomation.services;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.skilldistillery.frameworkautomation.entities.User;
 import com.skilldistillery.frameworkautomation.repositories.TemplateRepository;
 import com.skilldistillery.frameworkautomation.repositories.UserRepository;
 
@@ -14,5 +15,27 @@ public class UserServiceIMPL implements UserService {
 	
 	@Autowired
 	private TemplateRepository tempRepo;
+
+	@Override
+	public User findUserByID(int id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public User updateUser(User user, int id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean deleteUser(int id) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	
+	
+
 
 }
