2b1126fe88e09423980b8067acc9b12aafe867fd
wiseman-why-not <marcus.e.wiseman@gmail.com>
2020-03-02 09:56:05 -0700

    redesigned website

--- ngFrameworkAutomation/src/app/util/search/results/results.component.ts
+++ ngFrameworkAutomation/src/app/util/search/results/results.component.ts
@@ -1,7 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { TemplateService } from 'src/app/services/template.service';
 import { Template } from 'src/app/entities/template/template';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
+import { User } from 'src/app/entities/user/user';
+import { HttpClient } from '@angular/common/http';
+import { UserService } from 'src/app/services/user.service';
 
 @Component({
   selector: 'app-results',
@@ -10,9 +13,31 @@ import { ActivatedRoute } from '@angular/router';
 })
 export class ResultsComponent implements OnInit {
 
-  constructor(private templateService: TemplateService, private currentroute: ActivatedRoute) { }
+  // fields
+  user: User = new User();
+
+
+  constructor(private http: HttpClient, private svc: UserService,
+              private currentroute: ActivatedRoute, private router: Router,
+              private templateService: TemplateService) { }
+
+
+
+  getUserFromDB(){
+
+  }
+
+  // constructor(private templateService: TemplateService, private currentroute: ActivatedRoute) { }
 
   ngOnInit(): void {
+
+    this.svc.getUser().subscribe(
+      dataSuccess => {
+        this.user = dataSuccess;
+        console.log(this.user);
+      }
+    )
+
     let keywordResults = this.currentroute.queryParams.subscribe(
       queryParams => {
         console.log(queryParams);
