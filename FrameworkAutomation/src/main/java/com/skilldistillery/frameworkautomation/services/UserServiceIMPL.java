6110121601c9be587a90aed4561874e1a66d6764
wiseman-why-not <marcus.e.wiseman@gmail.com>
2020-03-02 20:30:03 -0700

    working on the nav bar

--- FrameworkAutomation/src/main/java/com/skilldistillery/frameworkautomation/services/UserServiceIMPL.java
+++ FrameworkAutomation/src/main/java/com/skilldistillery/frameworkautomation/services/UserServiceIMPL.java
@@ -3,6 +3,7 @@ package com.skilldistillery.frameworkautomation.services;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import com.skilldistillery.frameworkautomation.entities.User;
@@ -17,6 +18,9 @@ public class UserServiceIMPL implements UserService {
 
 	@Autowired
 	private TemplateRepository tempRepo;
+	
+	@Autowired
+	private PasswordEncoder encoder;
 
 	@Override
 	public User updateUser(User updatedUser) {
@@ -25,12 +29,18 @@ public class UserServiceIMPL implements UserService {
 
 		// check to see if updated user is ACTUALLY updated **************
 		User olduser = repo.findByUsernameAndEnabledTrue(updatedUser.getUsername());
-		
+		System.out.println(updatedUser);
 		if (updatedUser.getEmail() != null) {
 			olduser.setEmail(updatedUser.getEmail());
 		}
 		if (updatedUser.getOrganizationName() != null) {
 			olduser.setOrganizationName(updatedUser.getOrganizationName());
+		}
+		if (updatedUser.getPassword() != null) {
+			System.out.println("hello im in password");
+			String encodedPW = encoder.encode(updatedUser.getPassword());
+			olduser.setPassword(encodedPW); // only persist encoded password
+
 		}
 		repo.saveAndFlush(olduser);
 		return olduser;
