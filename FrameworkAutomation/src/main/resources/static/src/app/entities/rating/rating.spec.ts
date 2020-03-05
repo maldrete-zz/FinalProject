9d6727bde9b1313cc417778298dfe0a8e2193d50
Luke Leonard (Carly) <lukeleonard.coder@gmail.com>
2020-03-04 21:31:05 -0700

    removed npm files

--- FrameworkAutomation/src/main/resources/static/src/app/entities/rating/rating.spec.ts
+++
@@ -1,7 +0,0 @@
-import { Rating } from './rating';
-
-describe('Rating', () => {
-  it('should create an instance', () => {
-    expect(new Rating()).toBeTruthy();
-  });
-});
--- FrameworkAutomation/src/main/resources/static/src/app/util/generator/myPipes/lsc.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { LSCPipe } from './lsc.pipe';
-
-describe('LSCPipe', () => {
-  it('create an instance', () => {
-    const pipe = new LSCPipe();
-    expect(pipe).toBeTruthy();
-  });
-});
